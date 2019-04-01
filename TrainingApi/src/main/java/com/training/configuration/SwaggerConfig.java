629a05ffb606dc6cefcf563221e2043528b6b34c
SUNLIFE_PH\IR15 <Nhut.Le@sunlife.com.vn>
2019-04-01 18:26:08 +0700

    configuration for swagger security authCode

--- TrainingApi/src/main/java/com/training/configuration/SwaggerConfig.java
+++ TrainingApi/src/main/java/com/training/configuration/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.training.configuration;
 import java.util.Arrays;
 import java.util.Set;
 
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,14 +15,14 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
 import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -42,6 +43,8 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public Docket kpiApi() {
 		return new Docket(DocumentationType.SWAGGER_2).groupName("teamKpi")
+				//.protocols(Collections.singleton("https"))
+				//.host("abc.cc")
 				.select()
 				.apis(allowGetRequestHandlers())
 				.paths(PathSelectors.ant("/training/**")).build()
@@ -70,11 +73,54 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 	
 	@Bean
 	public Docket customApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("Custom Export APIs")
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Custom APIs")
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.training.controller"))
 				.paths(PathSelectors.ant("/**"))
-				.build().apiInfo(metadata());
+				.build()
+				.securitySchemes(Arrays.asList(securityScheme()))
+				.securityContexts(Arrays.asList(securityContext()))
+				.apiInfo(metadata());
+	}
+
+	private AuthorizationScope[] scopes() {
+		AuthorizationScope[] scopes = {
+				new AuthorizationScope("read", "for read operations"),
+				new AuthorizationScope("write", "for write operations"),
+				new AuthorizationScope("foo", "Access foo API") };
+		return scopes;
+	}
+
+	private SecurityScheme securityScheme() {
+		GrantType grantType = new AuthorizationCodeGrantBuilder()
+				.tokenEndpoint(new TokenEndpoint("AUTH_SERVER" + "/token", "oauthtoken"))
+				.tokenRequestEndpoint(
+						new TokenRequestEndpoint("AUTH_SERVER" + "/authorize", "CLIENT_ID", "CLIENT_ID"))
+				.build();
+
+		SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+				.grantTypes(Arrays.asList(grantType))
+				.scopes(Arrays.asList(scopes()))
+				.build();
+		return oauth;
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(
+						Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+				.forPaths(PathSelectors.regex("/TrainingClassTest.*"))
+				.build();
+	}
+
+	@Bean
+	public SecurityConfiguration security() {
+		return SecurityConfigurationBuilder.builder()
+				.clientId("CLIENT_ID")
+				.clientSecret("CLIENT_SECRET")
+				.scopeSeparator(" ")
+				.useBasicAuthenticationWithAccessCodeGrant(true)
+				.build();
 	}
 	
 	@Bean
@@ -96,7 +142,7 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 	}
 
 	private ApiInfo metadata() {
-		return new ApiInfoBuilder().title("Development API Service")
+		return new ApiInfoBuilder().title("Development APIs Service")
 				.description("REST API for Training resource").version("0.0.1-SNAPSHOT")
 				.contact(new Contact("LBN Invidual", "http://www.lbn.com.vn", "nhutlbuit@gmail.com"))
 				.build();
